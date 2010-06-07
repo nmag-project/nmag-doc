c3a41b9484509c79f251ad688f8baf89a45a1fbd
Matteo Franchin [beta] <franchin@soton.ac.uk>
2010-06-07 12:20:56 +0100

    Clarifying that e is the absolute val of the electron charge. Also moving equations out of makefile.

--- nmag/latex-png.py
+++ nmag/latex-png.py
@@ -1,20 +1,45 @@
 #!/usr/bin/python
 
-import sys, os
-
-otheropts = ""
-if len(sys.argv) == 3:
-  _, filename, formula = sys.argv
-else:
-  try:
-    _, filename, formula, border = sys.argv
-    otheropts = "-border %s -bordercolor white" % border
-  except:
+def usage():
     print "USAGE:  python latex-png.py outfile.png \"my = eq\" [border]"
     print "EXAMPLE 1: python latex-png.py sqrt.png \"c = \sqrt{a^2 + b^2}\""
     print "EXAMPLE 2: python latex-png.py my_equation.png \"c = \sqrt{a^2 + b^2}\" 5"
+    print "EXAMPLE 2: python latex-png.py -f equation.tex my_equation.png 5"
     sys.exit(1)
 
+def get(l, i, default=None):
+  if i < len(l):
+    return l[i]
+  elif default == None:
+    usage()
+  else:
+    return default
+
+import sys, os
+
+args = sys.argv[1:]
+l = len(args)
+if l < 1:
+  usage()
+
+border = False
+if args[0] == "-f":
+  assert l >= 2, \
+    "-f must be followed by the name of the file containing the equation"
+  f = open(args[1], "r")
+  formula = f.read().strip()
+  f.close()
+
+  filename = get(args, 2)
+  border = get(args, 3, False)
+
+elif l == 2:
+  filename, formula = args
+
+elif l == 3:
+  filename, formula, border = args
+
+otheropts = "-border %s -bordercolor white" % border if border != False else ""
 tmpdir = "/tmp/latex-formula-%d" % os.getpid()
 os.mkdir(tmpdir)
 

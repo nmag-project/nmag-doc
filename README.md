f3f33b143e32c07d0856d3f292abe0f756bf92d3
Madison Hanberry <mhanberry1@gmail.com>
2025-08-14 01:33:48 -0700

    host documentation on github pages

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# Readme
+# nmag-docs
 
 This repository contains documentation for the micromagnetic simulation package Nmag. The user documentation can
 be found [here](http://nmag.readthedocs.io/en/latest/)
+
+## Dependencies
+
+- docker
+
+## Building
+
+`docker compose up`
--- nmag/source/conf.py
+++ nmag/source/conf.py
@@ -31,6 +31,7 @@
 # ones.
 extensions = [
     'sphinx.ext.mathjax',
+	'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -121,7 +122,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
